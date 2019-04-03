aa1ce2e688466509de8baede509b83a9e39687ca
Nasy <nasyxx+git@gmail.com>
2019-04-03 19:56:16 +0800

    Change jieba to thulac.

--- bot.py
+++ bot.py
@@ -38,29 +38,30 @@ There are more things in heaven and earth, Horatio, than are dreamt.
  --  From "Hamlet"
 """
 # Other Packages
-from jieba import lcut as lc
-from jieba import initialize
+import thulac
 from telegram import InputTextMessageContent, InlineQueryResultArticle
 from telegram.ext import Updater, CallbackContext, InlineQueryHandler
 from telegram.update import Update
 
 from config import BOT_TOKEN
 
-initialize()
+Words = thulac.thulac(seg_only=True).cut  # pylint: disable=C0103
 
 
 def words(update: Update, context: CallbackContext) -> None:
     """Words the inline message."""
     query = update.inline_query.query
-    res = " ".join(lc(query, cut_all=False))
-    print(query)
-    print(res, end="\n\n")
+    res = Words(query, text=True)
+    print("--" * 10)
+    print(query, res, sep="\n", end="\n")
+    print("--" * 10, end="\n\n")
     context.bot.answer_inline_query(
         update.inline_query.id,
         [
             InlineQueryResultArticle(
                 id=query[:5],
                 title="Words",
+                description=res,
                 input_message_content=InputTextMessageContent(res),
             )
         ],
