6f47905b7c5a91faf7ca6155a3bb1296d92f92ab
Nasy <nasyxx+git@gmail.com>
2019-04-03 20:11:47 +0800

    Add jieba pkuseg.  🤣

--- bot.py
+++ bot.py
@@ -38,32 +38,57 @@ There are more things in heaven and earth, Horatio, than are dreamt.
  --  From "Hamlet"
 """
 # Other Packages
-import thulac
+from jieba import lcut as jc
+from jieba import initialize
+from pkuseg import pkuseg
+from thulac import thulac
 from telegram import InputTextMessageContent, InlineQueryResultArticle
 from telegram.ext import Updater, CallbackContext, InlineQueryHandler
 from telegram.update import Update
 
 from config import BOT_TOKEN
 
-Words = thulac.thulac(seg_only=True).cut  # pylint: disable=C0103
+initialize()
+
+tc = thulac(seg_only=True).cut  # pylint: disable=C0103
+pc = pkuseg().cut  # pylint: disable=C0103
 
 
 def words(update: Update, context: CallbackContext) -> None:
     """Words the inline message."""
     query = update.inline_query.query
-    res = Words(query, text=True)
+
+    resj = " ".join(jc(query, cut_all=False))
+    rest = tc(query, text=True)
+    resp = " ".join(pc(query))
+
     print("--" * 10)
-    print(query, res, sep="\n", end="\n")
+    print(query, end="\n")
+    print("-" * 10)
+    print("jieba: " + resj, "thulac: " + rest, "pkuseg: " + resp, sep="\n")
     print("--" * 10, end="\n\n")
+
     context.bot.answer_inline_query(
         update.inline_query.id,
         [
             InlineQueryResultArticle(
-                id=query[:5],
-                title="Words",
-                description=res,
-                input_message_content=InputTextMessageContent(res),
-            )
+                id=query[:5] + "|j",
+                title="jieba",
+                description=resj,
+                input_message_content=InputTextMessageContent(resj),
+            ),
+            InlineQueryResultArticle(
+                id=query[:5] + "|t",
+                title="thulac",
+                description=rest,
+                input_message_content=InputTextMessageContent(rest),
+            ),
+            InlineQueryResultArticle(
+                id=query[:5] + "|p",
+                title="pkuseg",
+                description=resp,
+                input_message_content=InputTextMessageContent(resp),
+            ),
         ],
     )
 
